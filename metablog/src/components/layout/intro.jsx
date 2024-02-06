283907d36c271635e5b336faffd9c02cd4a68ed5
bvme <gumbee.1031@gmail.com>
2024-02-06 12:10:50 +0800

    idc

--- metablog/src/components/layout/intro.jsx
+++ metablog/src/components/layout/intro.jsx
@@ -1,9 +1,8 @@
 import { First, Second, Third } from "../UI/introimg";
-import { carousel } from "flowbite ";
 
 export const Intro = () => {
   return (
-    <div className="flex justify-center items-center px-[300px]" id=" default-carousel " data-carousel="slidegit">
+    <div className="flex justify-center items-center px-[300px]">
       <First />
       <Second />
       <Third />
