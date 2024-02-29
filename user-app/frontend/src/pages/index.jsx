9afaf9134281ecd7b81839af070159f01be8ec29
Misheel burenzul <23LP7177@G304-CC0083.local>
2024-02-29 17:54:49 +0800

    2/29

--- user-app/frontend/src/pages/index.jsx
+++ user-app/frontend/src/pages/index.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
+const API_URL = "http://localhost:8080/";
 export default function Home() {
-  const API_URL = "http://localhost:8080/";
+  const [userData, setUserData] = useState({
+    name: "",
+    age:null,
+  })
   const getUsers = async()=>{
     try {
       const res = fetch(API_URL);
@@ -9,23 +15,30 @@ export default function Home() {
       console.log(e);
     }
   }
-   const handleSubmit = async()=>{
+  const handleSubmit = async()=>{
     try {
       const res = fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify
+        headers:{
+          Accept:"application/json",
+          "content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          name: "bvme",
+          age: "21",
+        })
       });
       const data = await res.json();
       console.log(data);
     } catch (e) {
       console.log(e);
     }
-   }
+  }
   return (
     <div className="flex flex-row gap-[10px] bg-blue-300 w-[500px] h-[500px]">
-      <input className=" h-6" type="text" />
+      <input className=" h-6" type="text" onChange={setUserData} />
       <input className=" h-6" type="number" />
-      <button className="h-6 bg-blue-100" onClick={handleSubmit}>submit</button>
+      <button className="h-6 bg-blue-100" onClick={getUsers}>submit</button>
     </div>
   );
 }
